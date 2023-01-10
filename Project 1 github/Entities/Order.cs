50213e135913988866452a3163484012c018ad62
Filipe Peres <filiperesloja03@icloud.com>
2023-01-09 21:39:00 -0300

    Project Enum

---
+++ Project 1 github/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Project_1_github.Entities.Enuns;
+
+namespace Project_1_github.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id 
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+
+
+    }
+}
---
+++ Project 1 github/Program.cs
@@ -0,0 +1,39 @@
+﻿using Project_1_github.Entities;
+using Project_1_github.Entities.Enuns;
+
+namespace Project_1_github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order()
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+            Console.WriteLine(OrderStatus.PendingPayment);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
